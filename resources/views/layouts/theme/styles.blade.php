81722cf4bbcc1daf1de93d88c623f42b0c641d9f
enriquesousa <enrique.sousa@gmail.com>
2021-12-11 22:21:37 -0600

    14. Personalizar Plugins

--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -3,12 +3,52 @@
 
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+
+{{-- <link href="{{ asset('bootstrap/css/bootstrap.css') }}" rel="stylesheet" type="text/css" /> --}}
 <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+
 <link href="{{ asset('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
+
+<link href="{{ asset('plugins/font-icons/fontawesome/css/fontawesome.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" type="text/css"/>
+
+<link href="{{ asset('assets/css/elements/avatar.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('plugins/sweetalerts/sweetalert.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('plugins/notification/snackbar/snackbar.min.css') }}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/widgets/modules-widgets.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/forms/theme-checkbox-radio.css') }}">
+
+<style>
+    aside{
+        display: none!important;
+    }
+    .page-item.active .page-link {
+        z-index: 3;
+        color: #fff;
+        background-color: #3b3f5c;
+        border-color: #3b3f5c;
+    }
+
+    @media (max-width: 480px){
+        .mtmobile {
+            margin-bottom: 20px!important;
+        }
+        .mbmobile {
+            margin-bottom: 10px!important;
+        }
+        .hideonsm {
+            display: none!important;
+        }
+        .inblock {
+            display: block;
+        }
+    }
+</style>
 <!-- END GLOBAL MANDATORY STYLES -->
 
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
-<link href="{{ asset('plugins/apex/apexcharts.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('assets/css/dashboard/dash_2.css') }}" rel="stylesheet" type="text/css" class="dashboard-sales" />
-<!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+@livewireStyles
