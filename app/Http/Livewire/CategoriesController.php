bdce4b93d1fc4b83513c30f5a6bf2e65a93669fb
enriquesousa <enrique.sousa@gmail.com>
2021-12-18 09:56:23 -0800

    21. Primer Componente Categories

---
+++ app/Http/Livewire/CategoriesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
+
+use Livewire\WithFileUploads; // trait para subir imagenes al backend
+use Livewire\WithPagination;
+
+class CategoriesController extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selected_id, $pageTitle, $componentName;
+    private $pagination = 5;
+
+    public function render()
+    {
+        $data = Category::all();
+
+        return view('livewire.category.categories', ['categories' => $data])
+            ->extends('layouts.theme.app')
+            ->section('content');
+    }
+}
