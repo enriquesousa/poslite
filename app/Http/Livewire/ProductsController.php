e1da55f01555d05879991e37c61510fe5a864bff
enriquesousa <enrique.sousa@gmail.com>
2021-12-22 20:02:29 -0800

    35. Programacion Part 2

--- app/Http/Livewire/ProductsController.php
+++ app/Http/Livewire/ProductsController.php
@@ -51,6 +51,54 @@ class ProductsController extends Component
 
    public function Store()
    {
-       # code...
+        $rules = [
+            'name' => 'required|unique:products|min:3',
+            'cost' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'alerts' => 'required',
+            'categoryid' => 'required|not_in:Elegir'
+        ];
+        $messages = [
+            'name.required' => 'Nombre del Producto es requerido',
+            'name.unique' => 'Ya existe el nombre del producto',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres',
+            'cost.required' => 'El costo es requerido',
+            'price.required' => 'El precio es requerido',
+            'stock.required' => 'El stock es requerido',
+            'alerts.required' => 'Ingresa el valor mínima en existencias',
+            'categoryid.not_in' => 'Elige un nombre de categoría diferente de Elegir',
+        ];
+        $this->validate($rules, $messages);
+
+        $product = Product::create([
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'price' => $this->price,
+            'barcode' => $this->barcode,
+            'stock' => $this->stock,
+            'alerts' => $this->alerts,
+            'category_id' => $this->categoryid,
+        ]);
+
+        $customFilename;
+        if($this->image){
+            $customFilename = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/products', $customFilename);
+            $product->image = $customFilename;
+            $product->save();
+        }
+
+        $this->resetUI(); // Limpiar las cajas de texto del formulario
+        $this->emit('product-added','Producto Registrado');
    }
+
+   // Para poder cerrar la ventana modal
+   public function resetUI(){
+        // $this->name = '';
+        // $this->image = null;
+        // $this->search = '';
+        // $this->selected_id = 0;
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,4 +25,22 @@ class Product extends Model
     {
         return $this->belongTo(Category::class);
     }
+
+    public function getImageAttribute($image){
+
+        // Segun otra forma de hacerlo, no me salio a mi 
+        // if($this->image != null)
+        //     return (file_exists('storage/products/' . $this->image) ? $this->image : 'noimg.jpg');
+        // else
+        //     return 'noimg.jpg';
+
+        // Otra forma de hacerlo seria, esta si me funciono a mi:
+        if (file_exists('storage/products/' . $image)) {
+            return $image;
+        }else{
+            return 'noimg.jpg';
+        }
+
+    }
+
 }
