ae85944fd3a9188c538fb0ad989e329903d6a214
enriquesousa <enrique.sousa@gmail.com>
2021-12-22 23:31:29 -0800

    36. Programacion Part 3 Update

--- app/Http/Livewire/ProductsController.php
+++ app/Http/Livewire/ProductsController.php
@@ -93,6 +93,77 @@ class ProductsController extends Component
         $this->emit('product-added','Producto Registrado');
    }
 
+   public function Edit(Product $product)
+   {
+        $this->selected_id = $product->id;
+        $this->name = $product->name;
+        $this->barcode = $product->barcode;
+        $this->cost = $product->cost;
+        $this->price = $product->price;
+        $this->stock = $product->stock;
+        $this->alerts = $product->alerts;
+        $this->categoryid = $product->category_id;
+        $this->image = null;
+
+        $this->emit('modal-show', 'Show modal');
+    }
+
+    public function Update()
+    {
+        $rules = [
+            'name' => "required|min:3|unique:products,name,{$this->selected_id}",
+            'cost' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'alerts' => 'required',
+            'categoryid' => 'required|not_in:Elegir'
+        ];
+        $messages = [
+            'name.required' => 'Nombre del Producto es requerido',
+            'name.unique' => 'Ya existe el nombre del producto',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres',
+            'cost.required' => 'El costo es requerido',
+            'price.required' => 'El precio es requerido',
+            'stock.required' => 'El stock es requerido',
+            'alerts.required' => 'Ingresa el valor mínima en existencias',
+            'categoryid.not_in' => 'Elige un nombre de categoría diferente de Elegir',
+        ];
+        $this->validate($rules, $messages);
+
+        $product = Product::find($this->selected_id);
+
+        $product->update([
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'price' => $this->price,
+            'barcode' => $this->barcode,
+            'stock' => $this->stock,
+            'alerts' => $this->alerts,
+            'category_id' => $this->categoryid,
+        ]);
+
+        $customFilename;
+        if($this->image){
+            $customFilename = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/products', $customFilename);
+            $imageTemp = $product->image; //imagen temporal
+            $product->image = $customFilename;
+            $product->save();
+
+            if ($imageTemp != null) {
+                if (file_exists('storage/products' . $imageTemp)) {
+                    unlink('storage/products' . $imageTemp);
+                }
+            }
+
+        }
+
+    $this->resetUI(); // Limpiar las cajas de texto del formulario
+    $this->emit('product-updated','Producto Actualizado');
+
+   }
+
+
    // Para poder cerrar la ventana modal
    public function resetUI(){
         // $this->name = '';
