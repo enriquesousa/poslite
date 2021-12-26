9608a4578e45f2e5b7ad91ddd6c1e8ec7092d213
enriquesousa <enrique.sousa@gmail.com>
2021-12-26 09:47:44 -0800

    47. Programación Parte 1

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -3,15 +3,43 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 use App\Models\Denomination;
 
 class PosController extends Component
 {
-    public $total=10, $itemsQuantity=1, $cart = [], $denominations=[], $efectivo, $change;
+    public $total, $itemsQuantity, $efectivo, $change;
     
+    public function mount()
+    {
+        $this->efectivo;
+        $this->change;
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+    }
+
     public function render()
     {
         $this->denominations = Denomination::all();
-        return view('livewire.pos.component')->extends('layouts.theme.app')->section('content');
+        return view('livewire.pos.component', [
+            'denominations' => Denomination::orderBy('value', 'desc')->get(),
+            'cart' => Cart::getContent()->sortBy('name')
+        ])
+        ->extends('layouts.theme.app')->section('content');
     }
+
+    public function ACash($value)
+    {
+        $this->efectivo += ($value == 0 ? $this->total : $value);
+        $this->change = ($this->efectivo - $this->total);
+    }
+
+    protected $listeners = [
+        'scan-code' => 'ScanCode',
+        'removeItem' => 'removeItem',
+        'clearCart' => 'clearCart',
+        'saveSale' => 'saveSale'
+    ];
+
+
 }
