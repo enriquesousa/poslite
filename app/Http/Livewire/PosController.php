5b4cf679de71f0cfc1155226643dfc8adcc49a32
enriquesousa <enrique.sousa@gmail.com>
2021-12-25 19:02:35 -0800

    45. Vista Denominaciones

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Denomination;
 
 class PosController extends Component
 {
-    public $total=10, $itemsQuantity=1, $cart = [];
+    public $total=10, $itemsQuantity=1, $cart = [], $denominations=[], $efectivo, $change;
     
     public function render()
     {
+        $this->denominations = Denomination::all();
         return view('livewire.pos.component')->extends('layouts.theme.app')->section('content');
     }
 }
