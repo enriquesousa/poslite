31d7ec7f28d68d1b85f1ca2ce9ae31332b721000
enriquesousa <enrique.sousa@gmail.com>
2021-12-26 11:11:17 -0800

    51. Programación Parte 5 removeItem y decreaseQty

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -132,4 +132,26 @@ class PosController extends Component
         }
     }
 
+    public function removeItem($productId)
+    {
+        Cart::remove($productId);
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+        $this->emit('scan-ok', 'Producto Eliminado');
+    }
+
+    public function decreaseQty($productId)
+    {
+        $item = Cart::get($productId);
+        Cart::remove($productId);
+
+        $newQty = ($item->quantity) - 1;
+        if ($newQty > 0) {
+            Cart::add($item->id, $item->name, $item->price, $newQty, $item->attributes[0]);
+        }
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+        $this->emit('scan-ok', 'Cantidad Actualizada');
+    }
+
 }
